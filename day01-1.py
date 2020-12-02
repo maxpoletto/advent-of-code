6cff9230620cfa1ee1f789450da0f07b07c90851
Max Poletto <max.poletto@gmail.com>
2020-12-02 09:12:11 +0100

    minor cleanup (work with negative numbers too)

--- day01-1.py
+++ day01-1.py
@@ -8,12 +8,8 @@ with open(filename) as f:
     for l in f:
         n.append(int(l))
 n.sort()
-print(n)
 i, j = 0, len(n)-1
-while True:
-    if i >= j:
-        print("Not found")
-        exit(1)
+while i < j:
     s = n[i]+n[j]
     if s > target:
         j = j-1
@@ -22,3 +18,5 @@ while True:
     else:
         print(n[i], "and", n[j], "add up to", target, "and their product is", n[i]*n[j])
         exit(0)
+print("Not found")
+exit(1)
--- day01-2.py
+++ day01-2.py
@@ -8,15 +8,10 @@ with open(filename) as f:
     for l in f:
         n.append(int(l))
 n.sort()
-print(len(n))
 for k in range(len(n)-2):
-    if n[k] >= target:
-        break
     t = target-n[k]
     i, j = k+1, len(n)-1
-    while True:
-        if i >= j:
-            break
+    while i < j:
         s = n[i]+n[j]
         if s > t:
             j = j-1
@@ -26,3 +21,4 @@ for k in range(len(n)-2):
             print(", ".join([str(x) for x in sorted([n[i], n[j], n[k]])]), "add up to", target, "and their product is", n[i]*n[j]*n[k])
             exit(0)
 print("Not found")
+exit(1)
