3fcae821bed77ba899b57c8dc0df63a572f8736d
Massimiliano Poletto <max.poletto@gmail.com>
2020-12-24 09:03:52 +0100

    day 24, cleaned up

--- day24.py
+++ day24.py
@@ -2,111 +2,104 @@
 
 import copy
 
-def get_dirs(l):
-    d = []
-    while len(l) > 0:
-        if l[:1] == "e":
-            d.append([0,2])
-            l = l[1:]
-        elif l[:1] == "w":
-            d.append([0,-2])
-            l = l[1:]
-        elif l[:2] == "ne":
-            d.append([-1,1])
-            l = l[2:]
-        elif l[:2] == "nw":
-            d.append([-1,-1])
-            l = l[2:]
-        elif l[:2] == "se":
-            d.append([1,1])
-            l = l[2:]
-        elif l[:2] == "sw":
-            d.append([1,-1])
-            l = l[2:]
-    return d
-
 class Floor:
+    W, B, X = 0, 1, 2
     def __init__(self, size):
+        """Creates a new tiled floor with at least size tiles in every direction
+        from the center."""
         h = 2*(size+1)+1
-        w = 2*(2*(size+1)+1)
+        w = 2*h
         self.zr = int(h/2)
         self.zc = int(w/2)+self.zr%2-1
-        self.grid = [['.' for c in range(w)] for r in range(h)]
+        self.grid = [[self.X for c in range(w)] for r in range(h)]
         for r in range(h):
             for c in range(r%2, w, 2):
-                self.grid[r][c] = 'w'
-    def run(self, instrs):
+                self.grid[r][c] = self.W
+    def flip_tiles(self, instrs):
+        """For each list of directions in instrs, flips the tile reached by
+        following those directions starting at the center tile.
+        """
         for dirs in instrs:
             r, c = self.zr, self.zc
             for d in dirs:
                 r += d[0]
                 c += d[1]
-            assert(self.grid[r][c] == 'b' or self.grid[r][c] == 'w')
-            if self.grid[r][c] == 'b':
-                self.grid[r][c] = 'w'
-            elif self.grid[r][c] == 'w':
-                self.grid[r][c] = 'b'
+            assert(self.grid[r][c] != self.X)
+            self.grid[r][c] = 1 - self.grid[r][c]
     def nn(self, r, c, v):
+        """Returns number of neighbors of (r,c) that have value v."""
         n = 0
         for d in [[0,2],[0,-2],[-1,1],[-1,-1],[1,1],[1,-1]]:
             n += (self.grid[r+d[0]][c+d[1]] == v)
         return n
     def update(self):
+        """Flips every tile that has a certain combination of neighbors."""
         g2 = copy.deepcopy(self.grid)
         for r in range(1, len(self.grid)-1):
-            for c in range(2, len(self.grid[0])-2):
+            for c in range(2+r%2, len(self.grid[0])-2, 2):
                 v = self.grid[r][c]
-                if v == '.':
-                    continue
-                n = self.nn(r, c, 'b')
-                if v == 'b' and (n == 0 or n > 2):
-                    g2[r][c] = 'w'
-                if v == 'w' and n == 2:
-                    g2[r][c] = 'b'
+                assert(v != self.X)
+                n = self.nn(r, c, self.B)
+                if (v == self.B and (n == 0 or n > 2)) or (v == self.W and n == 2):
+                    g2[r][c] = 1 - g2[r][c]
         self.grid = g2
     def count(self, v):
+        """Returns the number of tiles of color v."""
         n = 0
         for r in self.grid:
-            for c in r:
-                if c == v:
-                    n += 1
+            n += r.count(v)
         return n
     def __str__(self):
-        res = "{} {}\n".format(len(self.grid), len(self.grid[0]))
         for r in range(len(self.grid)):
             for c in range(len(self.grid[r])):
+                x = ['w','b','.'][self.grid[r][c]]
                 if r == self.zr and c == self.zc:
-                    res += '*'
-                else:
-                    res += self.grid[r][c]
+                    x = x.upper()
+                res += x
             res += "\n"
         return res
+    @classmethod
+    def parse_dirs(_, l):
+        d = []
+        while len(l) > 0:
+            if l[:1] == "e":
+                d.append([0,2])
+                l = l[1:]
+            elif l[:1] == "w":
+                d.append([0,-2])
+                l = l[1:]
+            elif l[:2] == "ne":
+                d.append([-1,1])
+                l = l[2:]
+            elif l[:2] == "nw":
+                d.append([-1,-1])
+                l = l[2:]
+            elif l[:2] == "se":
+                d.append([1,1])
+                l = l[2:]
+            elif l[:2] == "sw":
+                d.append([1,-1])
+                l = l[2:]
+        return d
 
-def part1(instrs):
-    maxlen = 0
-    for i in instrs:
-        maxlen = max(maxlen,len(i))
+def part1(instrs, maxlen):
     f = Floor(maxlen)
-    f.run(instrs)
-    return f.count('b')
+    f.flip_tiles(instrs)
+    return f.count(Floor.B)
 
-def part2(instrs):
-    maxlen = 0
-    for i in instrs:
-        maxlen = max(maxlen,len(i))
-    f = Floor(maxlen)
-    f.run(instrs)
-    for i in range(100):
+def part2(instrs, maxlen):
+    f = Floor(maxlen+100)
+    f.flip_tiles(instrs)
+    for _ in range(100):
         f.update()
-        print(i+1, f.count('b'))
-    return f.count('b')
+    return f.count(Floor.B)
 
 fn = "input24.txt"
-fn = "w"
 with open(fn) as f:
-    instrs = []
+    instrs, maxlen = [], 0
     for l in f:
-        instrs.append(get_dirs(l.strip()))
+        instrs.append(Floor.parse_dirs(l.strip()))
+        maxlen = max(maxlen, len(instrs[-1]))
 
-print(part1(instrs))
-print(part2(instrs))
+print(part1(instrs, maxlen))
+print(part2(instrs, maxlen))
