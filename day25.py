4592091dc04d406acdcc1438cf9261d2a8afb58d
Massimiliano Poletto <max.poletto@gmail.com>
2020-12-25 09:35:15 +0100

    day 25. what a fun month!

---
+++ day25.py
@@ -0,0 +1,23 @@
+#!/opt/local/bin/python
+
+class Card:
+    def __init__(self, pub):
+        self.pub = pub
+        val, ls = 1, 0
+        while val != pub:
+            ls += 1
+            val = (val * 7) % 20201227
+        self.priv = ls
+    def ek(self, pub):
+        val = 1
+        for _ in range(self.priv):
+            val = (val * pub) % 20201227
+        return val
+
+pubs = [6270530, 14540258]
+keys = [Card(p) for p in pubs]
+ans = []
+for i in range(len(keys)):
+    ans.append(keys[i].ek(keys[1-i].pub))
+assert(ans[0] == ans[1])
+print(ans[0])
