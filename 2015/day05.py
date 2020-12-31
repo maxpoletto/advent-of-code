40977f4f7d2e17f84ae84cb58de276f6cbb9ee6a
Massimiliano Poletto <max.poletto@gmail.com>
2020-12-31 01:57:50 +0100

    2015/5

---
+++ 2015/day05.py
@@ -0,0 +1,31 @@
+#!/opt/local/bin/python
+
+from lib import aodfile
+import re
+
+def part1(lines):
+    nice = 0
+    for l in lines:
+        if re.match(r'.*(?:ab|cd|pq|xy)', l):
+            continue
+        if not re.match(r'.*([a-z])\1', l):
+            continue
+        if not re.match(r'(.*[aeiou]){3,}', l):
+            continue
+        nice += 1
+    return nice
+
+def part2(lines):
+    nice = 0
+    for l in lines:
+        if not re.match(r'.*([a-z][a-z]).*\1', l):
+            continue
+        if not re.match(r'.*([a-z])[a-z]\1', l):
+            continue
+        nice += 1
+    return nice
+
+fn = "input/input05.txt"
+lines = aodfile.stripped_lines(fn)
+print(part1(lines))
+print(part2(lines))
--- lib/aodfile.py
+++ lib/aodfile.py
@@ -4,3 +4,10 @@ def stripped_text(fn):
     with open(fn) as f:
         text = ''.join([x.strip() for x in f.readlines()])
     return text
+
+def stripped_lines(fn):
+    lines = []
+    with open(fn) as f:
+        for l in f:
+            lines.append(l.strip())
+    return lines
