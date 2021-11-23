8dec5459bf53f4bf5c134942c0b2285fa8dc2523
Massimiliano Poletto <max.poletto@gmail.com>
2021-11-23 22:56:07 +0100

    fast and compact decompressor

--- 2016/day09.py
+++ 2016/day09.py
@@ -1,47 +1,30 @@
 import re
 from lib import aodfile
 
-def part1():
+def expand(l, rec) -> str:
+    pos, cnt = 0, 0
+    m = re.finditer('\((\d+)x(\d+)\)', l)
+    for g in m:
+        lp, rp = g.start(), g.end()
+        seqlen, seqcnt = int(g.group(1)), int(g.group(2))
+        if lp < pos:
+            continue
+        if rec:
+            toexp = l[rp:rp+seqlen]
+            cnt += lp-pos + expand(toexp, rec)*seqcnt
+        else:
+            cnt += lp-pos + seqlen*seqcnt
+        pos = rp+seqlen
+    if pos < len(l):
+        cnt += len(l)-pos
+    return cnt
+
+def run(rec):
     lines = aodfile.stripped_lines("input/input09.txt")
-#    lines = aodfile.stripped_lines("x")
+    sum = 0
+    for l in lines:
+        sum += expand(l, rec)
+    print(sum)
 
-    cnt = 0
-    for l1 in lines:
-        pos = 0
-        l2 = ""
-        m = re.finditer('\((\d+)x(\d+)\)', l1)
-        for g in m:
-            lp, rp = g.start(), g.end()
-            seqlen, seqcnt = int(g.group(1)), int(g.group(2))
-            if lp < pos:
-                continue
-            l2 += l1[pos:lp] + l1[rp:rp+seqlen]*seqcnt
-            pos = rp+seqlen
-        if pos < len(l1):
-            l2 += l1[pos:len(l1)]
-        cnt += len(l2)
-    print(cnt)
-
-def part1():
-    lines = aodfile.stripped_lines("input/input09.txt")
-#    lines = aodfile.stripped_lines("x")
-
-    cnt = 0
-    for l1 in lines:
-        pos = 0
-        l2 = ""
-        m = re.finditer('\((\d+)x(\d+)\)', l1)
-        for g in m:
-            lp, rp = g.start(), g.end()
-            seqlen, seqcnt = int(g.group(1)), int(g.group(2))
-            if lp < pos:
-                continue
-            l2 += l1[pos:lp] + l1[rp:rp+seqlen]*seqcnt
-            pos = rp+seqlen
-        if pos < len(l1):
-            l2 += l1[pos:len(l1)]
-        cnt += len(l2)
-    print(cnt)
-
-part1()
-part2()
+run(False)
+run(True)
