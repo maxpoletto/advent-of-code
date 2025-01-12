21ee9a0c758028c912ec3807beb0fb70e4e33fcd
Massimiliano Poletto <max.poletto@gmail.com>
2025-01-12 22:57:48 +0100

    30x faster with topological sort

--- 2024/d24.py
+++ 2024/d24.py
@@ -1,35 +1,33 @@
+from collections import deque
 from operator import xor, and_, or_
-from pprint import pprint
-
 _funcs = { 'XOR': xor, 'AND': and_, 'OR': or_ }
-_gates = {}
-class Gate:
-    def __init__(self, a, b = None, op = None):
-        self.a, self.b, self.op = a, b, op
-    def eval(self):
-        if self.op is None:
-            return self.a
-        return _funcs[self.op](_gates[self.a].eval(), _gates[self.b].eval())
-
-def read_input():
-    global _gates
-    with open('input/i24.txt') as f:
-        for l in f:
+class Adder:
+    def __init__(self, s):
+        lines = s.split('\n')
+        self.gates, self.values = {}, {}
+        for l in lines:
             if ':' in l:
                 n, v = l.strip().split(': ')
-                _gates[n] = Gate(int(v))
+                self.values[n] = int(v)
             elif '-' in l:
                 x = l.strip().split(' ')
-                in1, in2, func, out = x[0], x[2], x[1], x[4]
-                _gates[out] = Gate(in1, in2, func)
+                in1, func, in2, out = x[0], x[1], x[2], x[4]
+                self.gates.setdefault(in1, []).append((in2, out, func))
+                self.gates.setdefault(in2, []).append((in1, out, func))
+    def calc(self):
+        q = deque(self.values)
+        while q:
+            in1 = q.popleft()
+            for in2, out, func in self.gates.get(in1, []):
+                if in2 in self.values and out not in self.values:
+                    self.values[out] = _funcs[func](self.values[in1], self.values[in2])
+                    q.append(out)
+        zs = sorted([k for k in self.values if k[0] == 'z'])
+        return sum(1 << i for i, z in enumerate(zs) if self.values[z])
 
 def part1():
-    zs = sorted([ g for g in _gates if g[0] == 'z'], reverse=True)
-    v = 0
-    for z in zs:
-        v <<= 1
-        v |= _gates[z].eval()
-    print(v)
+    with open('input/i24.txt') as f:
+        adder = Adder(f.read())
+        print(adder.calc())
 
-read_input()
 part1()
